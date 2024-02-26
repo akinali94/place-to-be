44e02a9431c43a39e3ccff420462c819057d4902
Ali Akin <aliakn.20@gmail.com>
2024-02-26 18:09:31 +0300

    Added Navigation  Components

---
+++ front-end/src/shared/components/UIElements/Avatar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Avatar.css";
+
+const Avatar = (props) => {
+  return (
+    <div className={`avatar ${props.className}`} style={props.style}>
+      <img
+        src={props.image}
+        alt={props.alt}
+        style={{ width: props.width, height: props.width }}
+      />
+    </div>
+    // <div className={`avatar ${props.className}`} style={props.style}>
+    //   <img
+    //     src={props.image}
+    //     alt={props.alt}
+    //     style={{ width: props.width, height: props.width }}
+    //   />
+    // </div>
+  );
+};
+
+export default Avatar;
